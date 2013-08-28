28c130344477b28352886b8984a36034da52fa85
leebeomjae <beomjae@gmail.com>
2013-08-28 14:08:38 +0900

    add rails install script for ubuntu13.04

---
+++ install_rails4_on_ubuntu13_04.sh
@@ -0,0 +1,39 @@
+sudo apt-get update
+
+sudo apt-get install vim
+
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev 
+libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev
+
+cd
+git clone git://github.com/sstephenson/rbenv.git .rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+
+
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install 2.0.0-p247
+rbenv global 2.0.0-p247
+ruby -v
+
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+
+git config --global color.ui true
+git config --global user.name "leebeomjae"
+git config --global user.email "beomjae@gmail.com"
+ssh-keygen -t rsa -C "beomjae@gmail.com"
+
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+
+gem install rails
+
+rbenv rehash
+
+rails -v
